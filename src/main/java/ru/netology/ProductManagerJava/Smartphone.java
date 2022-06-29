01b04d3f80a4b481618190cb1824cf58fb7d091d
VictoriaTatarinova <chiarouomo@gmail.com>
2022-06-29 22:58:09 +0200

    Added Product, Book, Smartphone,ProductRepository, ProductManager, ProductManagerTest, updated Pom.xml

---
+++ src/main/java/ru/netology/ProductManagerJava/Smartphone.java
@@ -0,0 +1,23 @@
+package ru.netology.ProductManagerJava;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone() {
+    }
+
+    public Smartphone(int id, String name, int cost, String manufacturer) {
+        super(id, name, cost);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+
+}
---
+++ src/test/java/ru/netology/ProductManagerJava/ProductManagerTest.java
@@ -0,0 +1,105 @@
+package ru.netology.ProductManagerJava;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductManagerTest {
+
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    Product book = new Book(1, "Death on the Nile", 3, "Agatha Christie");
+    Product book1 = new Book(2, "The hound of the Baskervilles", 450, "Arthur Conan Doyle");
+    Product smartphone = new Smartphone(3, "iPhone 12", 30_000, "Apple");
+    Product book3 = new Book(3, "The girl with the dragon tattoo", 0, "Stieg Larsson");
+
+    @BeforeEach
+    public void SetUp() {
+        manager.add(book);
+        manager.add(book1);
+        manager.add(smartphone);
+        manager.add(book3);
+    }
+
+
+    @Test
+    public void shouldFindAuthor() {
+        Product[] expected = {book};
+        Product[] actual = manager.searchBy("Agatha Christie");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindNameBook() {
+        Product[] expected = {book1};
+        Product[] actual = manager.searchBy("The hound of the Baskervilles");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindNameBookAndAuthor() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("The girl with the dragon tattoo" + "Stieg Larsson");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindNameBookAndId() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("The girl with the dragon tattoo" + 3);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindPhoneName() {
+        Product[] expected = {smartphone};
+        Product[] actual = manager.searchBy("iPhone 12");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindPhoneManufacturer() {
+        Product[] expected = {smartphone};
+        Product[] actual = manager.searchBy("Apple");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindPhoneManufacturerAndCost() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Apple" + 30_000);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindId() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("3");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindCost() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("30_000");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindCostNull() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("0");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindNull() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy(null);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
